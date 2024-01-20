238b008c9caaacac0c78c9c7dcf2216100c33239
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 21:29:17 +0800

    Add the instruction and pseudocode

---
+++ print_character_at_even_index.py
@@ -0,0 +1,13 @@
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# pseudocode
+
+# accept a string from the user
+
+# print the string
+
+# write a message printing only the even index characters
+
+# write a for loop to iterate through the string
+
+# print the characters at even index numbers
